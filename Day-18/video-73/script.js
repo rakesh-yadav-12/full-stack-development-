732c20391ae56efe7d7fd2b44bcbda4058965e91
Rakesh Yadav <ry7955724@gmail.com>
2026-02-09 11:34:09 +0530

    updated work of toaday

---
+++ Day-18/video-73/script.js
@@ -0,0 +1,7 @@
+function createCard(title, cName, views, monthsOld, duration){
+
+    // Finsh this
+
+}
+
+createCard("Introduction to Backend | Sigma web dev bideo #2", "codewithHarry", 56000, 7, "31:22", "https://i.ytimg.com/vi/7AgEjgUtho4/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLD5k1xvazrjSCkigB_ORxezpivKng")
