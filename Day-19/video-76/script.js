a4f88abd32831fb1e31f4a64f5331a58ecea5473
Rakesh Yadav <ry7955724@gmail.com>
2026-02-10 17:21:14 +0530

    updated

---
+++ Day-19/video-76/script.js
@@ -0,0 +1,29 @@
+// async function getData(){
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve(455)
+//     },3500);
+//   })  
+// }
+async function getData(){
+   let x =await fetch('https://jsonplaceholder.typicode.com/todos/1')
+     let data = await x.json()
+     console.log(data)
+}
+async function main() {
+    
+    console.log("Loading modules")
+    console.log("Do something else")
+    console.log("Load data")
+    console.log("Load data")
+    let data =await getData();
+    
+        console.log(data)
+        
+        console.log("process data")
+        
+        console.log("task 2")
+    
+}
+
+main();
